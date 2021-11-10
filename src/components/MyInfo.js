0cc4b1b9521cbc90de5fefb34df2f19485fd969f
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-11-10 17:03:03 +0530

    myInfo page done

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -3,60 +3,64 @@ import "../styles/myInfo.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
-// import { Box, IconButton, Link, Button } from "@material-ui/core";
 // import coding from "../images/2844250.jpg";
 import BlobSceneDark from "../assets/backgrounds/blob-scene-dark.svg";
+import Programmer2 from "../assets/programmer2.svg";
+import Button from "@mui/material/Button";
+import MessageIcon from '@mui/icons-material/Message';
+import CodeIcon from '@mui/icons-material/Code';
 
 function MyInfo() {
   return (
     <div className="myInfo">
-      <div className="realInfo">
-        <div className="name">
-          <span>Hi,</span>Abhishek
-        </div>
-        <div className="developer">A fullstack developer</div>
-        <div>
-          I enjoy solving complex problems. Frequently praised as
-          detail-oriented by my peers, I can be relied upon to help your company
-          achieve its goals by providing sustainable and scalable solutions.
+      <div className="bg">
+        <div className="realInfo">
+          <div className="hi">
+            <span> Hi,</span> I'm Abhishek
+          </div>
+          <div className="developer">A fullstack developer</div>
+
+          <div className="aboutMe">
+            I enjoy solving complex problems. Frequently praised as
+            detail-oriented by my peers, <br />I can be relied upon to help your
+            company achieve its goals by providing sustainable <br />
+            and scalable solutions.
+            <div className="Socialbtn">
+              <a
+                rel="noopener noreferrer"
+                href="https://github.com/abhishekchauhan15"
+                target="_blank"
+              >
+                <GitHubIcon />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/abhishekchauhan15/"
+                target="_blank"
+              >
+                <LinkedInIcon />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://twitter.com/1509_abhishek"
+                target="_blank"
+              >
+                <TwitterIcon />
+              </a>
+            </div>
+          </div>
           <div className="btn">
-            <a
-              rel="noopener noreferrer"
-              href="https://github.com/abhishekchauhan15"
-              target="_blank"
-            >
-              <GitHubIcon />
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/abhishekchauhan15/"
-              target="_blank"
-            >
-              <LinkedInIcon />
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="https://twitter.com/1509_abhishek"
-              target="_blank"
-            >
-              <TwitterIcon />
-            </a>
+            <Button variant="contained" startIcon={<MessageIcon />}>
+              GET IN TOUCH
+            </Button>
+            <Button variant="contained" startIcon={<CodeIcon />}>
+              SEE MY WORK
+            </Button>
           </div>
         </div>
-        <div className="btn">
-          <button>GET IN TOUCH</button>
-          <button>SEE MY WORK</button>
-        </div>
-      </div>
-      <div className="sideImg">
-        image
-        {/* <blobSceneDark /> */}
-       
-        <img src={BlobSceneDark} alt="blob" />
-
+        <img src={Programmer2} alt="background" className="sideImg" />
       </div>
       {/* <button>down button</button> */}
-      {/* <img src={coding} alt="Logo" /> */}
     </div>
   );
 }
